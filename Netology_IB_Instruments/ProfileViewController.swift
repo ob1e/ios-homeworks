20b044dd806c34a8cc16fc1163f6499457df449a
Sainchuk Nikolay <sainchuk.kolia@gmail.com>
2022-09-03 18:53:02 +0300

    add photo, name, location, text
    complete homework

---
+++ Netology_IB_Instruments/ProfileViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by JaY on 03.09.2022.
+//
+
+import Foundation
---
+++ Netology_IB_Instruments/XibFile/ProfileView.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by JaY on 31.08.2022.
+//
+
+import Foundation
