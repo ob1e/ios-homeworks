7705bdb96efa410ab05c0349719db632640baaaf
Sainchuk Nikolay <sainchuk.kolia@gmail.com>
2022-09-12 21:34:45 +0300

    complete homework

--- Netology_IB_Instruments/ProfileViewController.swift
+++ Netology_IB_Instruments/ProfileViewController.swift
@@ -5,4 +5,22 @@
 //  Created by JaY on 03.09.2022.
 //
 
-import Foundation
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemMint
+        loadXib()
+    }
+    
+    func loadXib() {
+        if let xibView = Bundle.main.loadNibNamed("ProfileView", owner: nil, options: nil)?.first as? ProfileView {
+            xibView.frame = CGRect(x: 0, y: 50, width: UIScreen.main.bounds.width, height: 600)
+            view.addSubview(xibView)
+        }
+    }
+}
+
+
--- Netology_IB_Instruments/XibFile/ProfileView.swift
+++ Netology_IB_Instruments/XibFile/ProfileView.swift
@@ -5,4 +5,15 @@
 //  Created by JaY on 31.08.2022.
 //
 
-import Foundation
+import UIKit
+
+class ProfileView: UIView {
+    
+    @IBOutlet weak var photo: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var birthday: UILabel!
+    @IBOutlet weak var location: UILabel!
+    @IBOutlet weak var text: UITextView!
+    
+}
+
